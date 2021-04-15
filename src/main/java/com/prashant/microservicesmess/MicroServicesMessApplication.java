7946d7b9c4e09d8cc3ca04b38539ee5e2f37c5d0
Prasahant Verma <v-prverma@expedia.com>
2021-04-15 10:41:18 +0530

    initial commit

---
+++ src/main/java/com/prashant/microservicesmess/MicroServicesMessApplication.java
@@ -0,0 +1,13 @@
+package com.prashant.microservicesmess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServicesMessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroServicesMessApplication.class, args);
+    }
+
+}
