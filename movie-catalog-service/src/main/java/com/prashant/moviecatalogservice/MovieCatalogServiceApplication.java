92ddef3555b08c0d9eb8c4c461932f1f368bba87
Prasahant Verma <v-prverma@expedia.com>
2021-04-18 15:28:17 +0530

    Adding timout to restTemplates

--- movie-catalog-service/src/main/java/com/prashant/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/com/prashant/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -2,9 +2,12 @@ package com.prashant.moviecatalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
 
 @SpringBootApplication
 public class MovieCatalogServiceApplication {
@@ -12,7 +15,10 @@ public class MovieCatalogServiceApplication {
     @Bean
     @LoadBalanced
     public RestTemplate getRestTemplate() {
-        return  new RestTemplate();
+        return  new RestTemplateBuilder()
+                .setConnectTimeout(Duration.ofSeconds(10))
+                .setReadTimeout(Duration.ofSeconds(10))
+                .build();
     }
 
     public static void main(String[] args) {
--- movie-info-service/src/main/java/com/prashant/movieinfoservice/MovieInfoServiceApplication.java
+++ movie-info-service/src/main/java/com/prashant/movieinfoservice/MovieInfoServiceApplication.java
@@ -2,9 +2,12 @@ package com.prashant.movieinfoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
 
 @SpringBootApplication
 public class MovieInfoServiceApplication {
@@ -15,7 +18,10 @@ public class MovieInfoServiceApplication {
 
     @Bean
     public RestTemplate getRestTemplate() {
-        return new RestTemplate();
+        return  new RestTemplateBuilder()
+                .setConnectTimeout(Duration.ofSeconds(10))
+                .setReadTimeout(Duration.ofSeconds(10))
+                .build();
     }
 
 }
